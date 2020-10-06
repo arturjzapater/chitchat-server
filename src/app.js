c2ca075edc7f097e8bde8aec77177ee98b9308c3
arturjzapater <zapater.artur@gmail.com>
2020-10-06 13:49:04 +0200

    added cors configuration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const usersApi = require('./users/routes')
+const { url } = require('../config/client')
 
 const { Router } = express
 const { NODE_ENV = 'development' } = process.env
@@ -13,7 +15,7 @@ const app = express()
 
 if (NODE_ENV !== 'test') app.use(morgan(logger))
 app.use(express.static(path.resolve('..', '..', 'client', 'dist')))
-app.use('/api/users', usersApi({ Router }))
+app.use('/api/users', cors({ origin: url }), usersApi({ Router }))
 
 app.get('/*', (req, res) => {
   NODE_ENV === 'production'
