05fbef5c7d4300cb5146806faf99c4ccbd20de20
arturjzapater <zapater.artur@gmail.com>
2020-10-06 17:08:08 +0200

    changed how isTyping works

--- src/chatroom/controller.js
+++ src/chatroom/controller.js
@@ -1,16 +1,29 @@
 const users = require('../users')
 const { timeout } = require('../../config/socket')
 
-const makeTimeout = socket => setTimeout(() => {
-  socket.emit('inactive')
-  socket.inactive = true
-  socket.disconnect(true)
-}, timeout)
+const makeInactiveTimeout = socket => {
+  if (socket.inactiveTimeout) clearTimeout(socket.inactiveTimeout)
+
+  return setTimeout(() => {
+    socket.emit('inactive')
+    socket.inactive = true
+    socket.disconnect(true)
+  }, timeout)
+}
+
+const makeTypingTimeout = (io, socket) => {
+  if (socket.typingTimeout) clearTimeout(socket.typingTimeout)
+
+  return setTimeout(() => {
+    users.update(socket.id, { isTyping: false })
+    io.emit('update userlist', users.list())
+  }, 2000)
+}
 
 const join = (io, socket) => name => {
   users.add(socket.id, name)
   socket.nickname = name
-  socket.timeout = makeTimeout(socket)
+  socket.inactiveTimeout = makeInactiveTimeout(socket)
 
   io.emit('update userlist', users.list())
   io.emit('new message', {
@@ -22,8 +35,7 @@ const join = (io, socket) => name => {
 }
 
 const newMessage = (io, socket) => message => {
-  clearTimeout(socket.timeout)
-  socket.timeout = makeTimeout(socket)
+  socket.inactiveTimeout = makeInactiveTimeout(socket)
 
   io.emit('new message', {
     user: socket.nickname,
@@ -33,8 +45,10 @@ const newMessage = (io, socket) => message => {
   })
 }
 
-const userTyping = (io, socket) => isTyping => {
-  users.update(socket.id, { isTyping })
+const userTyping = (io, socket) => () => {
+  socket.typingTimeout = makeTypingTimeout(io, socket)
+
+  users.update(socket.id, { isTyping: true })
   io.emit('update userlist', users.list())
 }
 
