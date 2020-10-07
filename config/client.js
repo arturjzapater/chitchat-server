bbf65643004283f5a0ec467c647fdce5335f2362
arturjzapater <zapater.artur@gmail.com>
2020-10-07 08:55:53 +0200

    updated home route

--- config/client.js
+++ config/client.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: 'https://chitchat-client.netlify.app'
+  url: process.env.NODE_ENV === 'production'
+    ? 'https://chitchat-client.netlify.app'
+    : 'http://localhost:3000'
 }
--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
-const path = require('path')
 const usersApi = require('./users/routes')
-const { url } = require('../config/client')
+const { url: clientUrl } = require('../config/client')
 
 const { Router } = express
 const { NODE_ENV = 'development' } = process.env
@@ -14,13 +13,11 @@ const logger = NODE_ENV === 'development'
 const app = express()
 
 if (NODE_ENV !== 'test') app.use(morgan(logger))
-app.use(express.static(path.resolve('..', '..', 'client', 'dist')))
-app.use('/api/users', cors({ origin: url }), usersApi({ Router }))
 
-app.get('/*', (req, res) => {
-  NODE_ENV === 'production'
-    ? res.sendFile(path.resolve('../../client/dist/index.html'))
-    : res.redirect('http://localhost:3000')
+app.get('/', (req, res) => {
+  res.redirect(clientUrl)
 })
+
+app.use('/api/users', cors({ origin: clientUrl }), usersApi({ Router }))
 
 module.exports = app
